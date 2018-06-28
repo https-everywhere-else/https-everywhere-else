0ec419feca71216cd78213ba98373e082cafc2b5
vi <vi@computational.law>
2018-06-28 01:45:41 -0600

    Cull tests against volatile interfaces.

--- test/Data/Text/ICU/ExtrasSpec.hs
+++ test/Data/Text/ICU/ExtrasSpec.hs
@@ -5,7 +5,6 @@
 module Data.Text.ICU.ExtrasSpec (spec) where
 
 import Control.Applicative ((<$>))
-import Control.Lens ((&))
 import Data.Text (Text)
 import Data.Text.ICU.Extras (match, findAndReplace, Segment(..), parseReplacement)
 import Test.Hspec (Spec, describe, it, shouldBe)
@@ -16,8 +15,8 @@ spec = do
     it "Should be Nothing if provided an invalid regular expression." $ do
       match "(" `shouldBe` Nothing
     it "Should yield a match function if provided a regular expression." $ do
-     ("xa" &) <$> match "x" `shouldBe` Just True
-     ("xa" &) <$> match "y" `shouldBe` Just False
+     ($ "xa") <$> match "x" `shouldBe` Just True
+     ($ "xa") <$> match "y" `shouldBe` Just False
   describe "parseReplacement" $ do
     it "Should decompose a replacement string into a sequence [Segment]." $ do
       parseReplacement "foo$1bar$4$1" `shouldBe` Just [Literal "foo", Reference 1, Literal "bar", Reference 4, Reference 1]
@@ -28,9 +27,9 @@ spec = do
       parseReplacement "$10" `shouldBe` Just [Reference 1, Literal "0"]
   describe "findAndReplace" $ do
     it "Should find and replace based upon a regular expression and pattern." $ do
-      ("barqux" &) <$> findAndReplace "(bar)" "$1baz" `shouldBe` Just (Just "barbazqux")
-      ("barqux" &) <$> findAndReplace "(qux)" "baz$1" `shouldBe` Just (Just "barbazqux")
-      ("barqux" &) <$> findAndReplace "u" "uu" `shouldBe` Just (Just "barquux")
+      ($ "barqux") <$> findAndReplace "(bar)" "$1baz" `shouldBe` Just (Just "barbazqux")
+      ($ "barqux") <$> findAndReplace "(qux)" "baz$1" `shouldBe` Just (Just "barbazqux")
+      ($ "barqux") <$> findAndReplace "u" "uu" `shouldBe` Just (Just "barquux")
 
 instance Show (Text -> Bool) where
   show _ = "Text -> Bool"
